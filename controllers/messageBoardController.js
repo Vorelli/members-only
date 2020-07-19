5f83213069e77c714d309e20ecae12a2bc749be5
Natale Toscano <kickalliance195@gmail.com>
2020-07-19 11:38:17 -0400

    Clean up style of front page and add post ability

--- controllers/messageBoardController.js
+++ controllers/messageBoardController.js
@@ -1,28 +1,42 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Message = mongoose.model('Message');
-const { body, validationResult, sanitize } = require('express-validator');
+const { sanitizeBody } = require('express-validator/filter');
+const { body, validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const async = require('async');
+const moment = require('moment');
 
 exports.index = function (req, res, next) {
-  const getMessages= {
-    messages: function(cb) { Message.find().populate({path: 'user', select: 'firstName lastName'}).exec(cb); }
-  }
-  const afterGet = function(err,results) {
-    if(err) next(err);
+  const getMessages = {
+    messages: function (cb) {
+      Message.find()
+        .populate({ path: 'user', select: 'firstName lastName' })
+        .exec(cb);
+    }
+  };
+  const afterGet = function (err, results) {
+    if (err) next(err);
     else {
-      if(!res.locals.currentUser) 
-        results.messages.forEach(message => { message.user = undefined })
-      else {
+      if (!res.locals.currentUser) {
+        results.messages.forEach((message) => {
+          message.user = undefined;
+        });
+      } else {
         for (var i = results.messages.length - 1; i >= 0; i--) {
-          results.messages[i].user = results.messages[i].user.firstName + ' ' + results.messages[i].user.lastName;
+          results.messages[i].user =
+            results.messages[i].user.firstName +
+            ' ' +
+            results.messages[i].user.lastName;
         }
       }
-      res.render('index', { title: 'Message Board', messages: results.messages })
+      res.render('index', {
+        title: 'Message Board',
+        messages: results.messages
+      });
     }
-  }
+  };
   async.parallel(getMessages, afterGet);
 };
 
@@ -31,7 +45,7 @@ exports.signUpGet = function (req, res, next) {
 };
 
 exports.signUpPost = [
-  sanitize('*').escape(),
+  sanitizeBody('*').escape(),
 
   body('username')
     .trim()
@@ -72,7 +86,7 @@ exports.signUpPost = [
       user.save({}, (err, theUser) => {
         if (err) next(err);
         else {
-          next()
+          next();
         }
       });
     }
@@ -88,7 +102,7 @@ exports.signInGet = function (req, res, next) {
 };
 
 exports.signInPost = [
-  sanitize('*').escape(),
+  sanitizeBody('*').escape(),
 
   body('username')
     .trim()
@@ -122,13 +136,51 @@ exports.signOutGet = function (req, res, next) {
   res.redirect('/');
 };
 
-exports.postGet = function(req, res, next) {
-  if(!res.locals.currentUser)
-    res.redirect('/');
+exports.postGet = function (req, res, next) {
+  if (!res.locals.currentUser) res.redirect('/');
   else {
-    res.render('postForm', {title: 'Create Post'})
+    res.render('postForm', { title: 'Create Post' });
   }
-}
+};
 
 exports.postPost = [
-]
+  sanitizeBody('messageBody').escape(),
+  sanitizeBody('title').escape(),
+
+  body('messageBody')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Message body is required.'),
+  body('title')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Message title is required.'),
+
+  (req, res, next) => {
+    if (res.locals.currentUser) {
+      const message = new Message({
+        title: req.body.title,
+        body: req.body.messageBody,
+        datePosted: moment(new Date()),
+        user: res.locals.currentUser
+      });
+
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        res.render('postForm', {
+          title: 'Create Post',
+          message: message,
+          errors: errors.array()
+        });
+      } else {
+        message.save({}, function (err, theMessage) {
+          if (err) next(err);
+          else res.redirect('/');
+        });
+      }
+    } else {
+      res.redirect('/');
+    }
+  }
+];
---
+++ public/javascripts/setTextarea.js
@@ -0,0 +1,3 @@
+const messageBody = document.currentScript.getAttribute('messageBody');
+
+document.querySelector('textarea').value = messageBody;
