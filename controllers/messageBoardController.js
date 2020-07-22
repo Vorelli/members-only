9ff0f67626caf1885a913a50cf189a64f15b81ee
Natale Toscano <kickalliance195@gmail.com>
2020-07-21 21:05:40 -0400

    Complete form validation middleware and file uploading

--- controllers/messageBoardController.js
+++ controllers/messageBoardController.js
@@ -7,6 +7,11 @@ const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const async = require('async');
 const moment = require('moment');
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
+const user = require('../models/user');
+const { raw } = require('express');
 
 exports.index = function (req, res, next) {
   const getMessages = {
@@ -45,55 +50,126 @@ exports.signUpGet = function (req, res, next) {
 };
 
 exports.signUpPost = [
-  sanitizeBody('*').escape(),
-
-  body('username')
-    .trim()
-    .isEmail()
-    .withMessage('Username needs to be an email.'),
-  body('firstName', 'You must supply a first name.')
-    .trim()
-    .isLength({ min: 1 }),
-  body('lastName', 'You must supply a last name.').trim().isLength({ min: 1 }),
-  body('password', 'Password must be supplied and at least 8 characters.')
-    .trim()
-    .isLength({ min: 8 })
-    .custom(
-      (value, { req, location, path }) =>
-        value === req.body.passwordConfirmation
-    ),
-
   (req, res, next) => {
-    const errors = validationResult(req);
+    res.locals.errors = [];
+    const form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      if (err) next(err);
+      else {
+        Object.keys(fields).forEach((key) => {
+          req.body[key] = fields[key];
+        });
+        if (files.picture.type !== 'image/jpeg') {
+          res.locals.errors.push({
+            msg: 'You must supply a valid jpg image.'
+          });
+        } else {
+          res.locals.files = files;
+        }
+      }
+      next();
+    });
+  },
 
-    const hashedPass = bcrypt.hashSync(req.body.password, 10);
+  sanitizeBody('firstName').escape(),
+  sanitizeBody('lastName').escape(),
+  sanitizeBody('username').escape(),
+
+  async (req, res, next) => {
+    req.body.hashedPass = bcrypt.hash(req.body.password, 10);
+
+    const username = req.body.username;
+    username.trim();
+    const emailValidator = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailValidator.test(username)) {
+      res.locals.errors.push({ msg: 'You must enter a valid email address.' });
+    }
+    if (await User.findOne({ username: username })) {
+      res.locals.errors.push({
+        msg: 'This email has been used before. Try to log in.'
+      });
+    }
+
+    const password = req.body.password;
+    const confirmPassword = req.body.passwordConfirmation;
+    if (password.length < 8 || password.length > 72) {
+      res.locals.errors.push({
+        msg:
+          'Your password needs to be 8 characters or longer as well as 72 characters or less.'
+      });
+    }
+    if (password !== confirmPassword) {
+      res.locals.errors.push({
+        msg: 'Your password and password confirmation must match.'
+      });
+    }
+
+    const firstName = req.body.firstName;
+    if (firstName.trim().length < 1) {
+      res.locals.errors.push({ msg: 'You must supply a first name.' });
+    }
+
+    const lastName = req.body.lastName;
+    if (lastName.trim().length < 1) {
+      res.locals.errors.push({ msg: 'You must supply a last name.' });
+    }
+    next();
+  },
+
+  async (req, res, next) => {
+    const errors = res.locals.errors;
+
     const user = new User({
       username: req.body.username,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
-      password: hashedPass,
+      password: await req.body.hashedPass,
       isMember: true,
       isAdmin: false
     });
 
-    if (!errors.isEmpty()) {
-      res.render('sign-up', {
-        title: 'Sign Up',
-        errors: errors.toArray(),
-        user: user
-      });
+    if (errors.length !== 0) {
+      if (
+        errors.some(function (value) {
+          return (
+            value.msg === 'This email has been used before. Try to log in.'
+          );
+        })
+      ) {
+        req.session.message = 'This email has been used before. Try to log in.';
+        res.redirect('/messageboard/sign-in');
+      } else {
+        res.render('sign-up', {
+          title: 'Sign Up',
+          errors: errors,
+          user: user
+        });
+      }
     } else {
       user.save({}, (err, theUser) => {
         if (err) next(err);
         else {
+          const files = res.locals.files;
+          const oldPath = files.picture.path;
+          const newPath = path.join(
+            path.join(__dirname, '../'),
+            'public/images/' + req.body.username + '.jpg'
+          );
+          const rawData = fs.readFileSync(oldPath);
+          console.log('done reading file');
+          fs.writeFileSync(newPath, rawData);
           next();
         }
       });
     }
   },
+  (req, res, next) => {
+    console.log('next!');
+    next();
+  },
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/'
+    failureRedirect: '/sign-up'
   })
 ];
 
